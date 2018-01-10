ccdc22a2317991b4bccad73fea2e6bb071ec1718
Oceatoon <oceatoon@gmail.com>
2018-01-10 13:04:07 +0400

    init ressources

---
+++ controllers/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * DefaultController.php
+ *
+ * OneScreenApp for Communecting people
+ *
+ * @author: Tibor Katelbach <tibor@pixelhumain.com>
+ * Date: 14/03/2014
+ */
+class DefaultController extends CommunecterController {
+
+
+  protected function beforeAction($action)
+	{
+    //parent::initPage();
+	  return parent::beforeAction($action);
+	}
+
+	public function actionIndex() 
+	{
+    	if(Yii::app()->request->isAjaxRequest)
+        echo $controller->renderPartial("index");
+      else
+      {
+        $this->layout = "//layouts/empty";
+        $this->render("index");
+      }
+  }
+
+  public function actionDoc() 
+  {
+      echo file_get_contents('../../modules/'.$this->module->id.'/README.md');
+  }
+
+}
---
+++ views/default/index.php
@@ -0,0 +1,39 @@
+<?php
+
+//assets from ph base repo
+$cssAnsScriptFilesTheme = array(
+	// SHOWDOWN
+	'/plugins/showdown/showdown.min.js',
+	//MARKDOWN
+	'/plugins/to-markdown/to-markdown.js'
+);
+HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesTheme, Yii::app()->request->baseUrl);
+
+//gettting asstes from parent module repo
+$cssAnsScriptFilesModule = array( 
+	'/js/dataHelpers.js',
+	'/css/md.css',
+);
+HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule, Yii::app()->getModule( Yii::app()->params["module"]["parent"] )->getAssetsUrl() );
+?>
+
+
+<h1 style="">
+	<img height=50 src="<?php echo Yii::app()->getModule( Yii::app()->params["module"]["parent"] )->getAssetsUrl()?>/images/CO.png">
+	<img height=50 src="<?php echo $this->module->assetsUrl?>/images/logo.png">
+	Learn Module
+</h1>
+
+<div id="doc"></div>
+
+<script type="text/javascript">
+
+$(document).ready(function() { 
+	getAjax('', baseUrl+'/'+moduleId+'/default/doc',
+		function(data){ 
+			descHtml = dataHelper.markdownToHtml(data); 
+			$('#doc').html(descHtml);
+		},"html");
+});
+
+</script>
