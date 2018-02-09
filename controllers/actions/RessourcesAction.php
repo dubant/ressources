8388dad8c19976d852783681c58b698a13f80abc
Kgneo <tristan.goguet@gmail.com>
2018-02-09 12:30:41 +1100

    count stats

--- controllers/actions/RessourcesAction.php
+++ controllers/actions/RessourcesAction.php
@@ -3,6 +3,7 @@ class RessourcesAction extends CAction
 {
     public function run()
     {
+      	CO2Stat::incNbLoad("co2-ressources");
 	   	echo $this->getController()->renderPartial("co2.views.app.search",array("type"=>"ressources"));
     }
 }
