8bd2d82f69f27388fad9fbc3c78d3f28f8a99aba
Kgneo <tristan.goguet@gmail.com>
2018-04-17 16:55:58 +1100

    up menu

--- views/co/categories.php
+++ views/co/categories.php
@@ -16,33 +16,50 @@
   font-size:12px;
 }
 </style>
+<?php 
+   /* ***********************
+        LIB FROM THIS MODULE (Classified)
+    ************************ */
+    $cssAnsScriptFilesModule = array('/assets/css/default/classified.css');
+    HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule, Yii::app()->theme->baseUrl);
+?>
+<div class="col-xs-12 col-sm-12 col-md-12" id="sectionMenu">
+  <div class="col-md-2 col-sm-2 col-xs-2">
+    <button class="btn btn-default bg-white col-md-12 col-sm-12 pull-right font-montserrat btn-select-type-anc letter-<?php echo @$section["color"]; ?> padding-10" 
+            data-type="classified" data-type-anc=""  data-key="all">
+      <i class="fa fa-refresh"></i>
+      <br><?php echo Yii::t("common","All"); ?>
+    </button>
+  </div>
+  <div class="col-md-10 col-sm-10 col-xs-10">
+    <?php 
+    $currentSection = 1;
+    foreach ($categories["sections"] as $key => $section) { ?>
 
-<div class="col-xs-12 col-sm-10 col-md-10" id="sectionMenu">
-
-  <button class="btn btn-link bg-white text-azure margin-left-5 btn-default btn-select-type-anc letter-<?php echo @$section["color"]; ?>" 
-          data-type="ressources" data-type-anc=""  data-key="all">
-    <i class="fa fa-circle-o"></i>
-    <span class="hidden-xs hidden-sm"> <?php echo Yii::t("common","Show all"); ?> </span>
-  </button> 
-
-  <?php 
-$currentSection = 1;
-foreach ($categories["sections"] as $key => $section) { ?>
-  <button class="btn btn-link bg-azure elipsis btn-select-type-anc" 
-          data-type-anc="<?php echo @$section["label"]; ?>" data-key="<?php echo @$section["key"]; ?>" 
-          data-type="ressources">
-    <i class="fa fa-<?php echo $section["icon"]; ?> hidden-xs"></i> 
-    <?php echo Yii::t("category", $section["labelFront"]); ?>
-  </button>
-<?php } ?> 
-
-  <button class="btn margin-left-5 btn-link bg-green-k btn-add" onclick="dyFObj.openForm('ressources')">
-    <i class="fa fa-plus"></i>
-    <span class="hidden-xs hidden-sm"> <?php echo Yii::t("common","Add"); ?> </span>
-  </button> 
+        <div class="col-md-4 col-sm-5 col-xs-3 no-padding">
+          <button class="btn btn-default col-md-12 col-sm-12 padding-10 font-montserrat elipsis btn-select-type-anc" 
+                data-type-anc="<?php echo @$section["label"]; ?>" data-key="<?php echo @$section["key"]; ?>" 
+                data-type="ressources">
+          <i class="fa fa-<?php echo $section["icon"]; ?>"></i><br>
+          <?php echo Yii::t("category", $section["labelFront"]); ?>
+        </button>
+      </div>
+    <?php } ?>
+      <div class="col-md-4 col-sm-2 col-xs-2 no-padding pull-right">
+          <button class="btn btn-default col-md-12 col-sm-12 bg-green-k padding-10 font-montserrat elipsis btn-add-classified btn-open-form" 
+                data-form-type="classifieds"
+                data-type-anc="<?php echo @$section["label"]; ?>" data-key="<?php echo @$section["key"]; ?>" 
+                data-type="ressources">
+          <i class="fa fa-plus-circle hidden-xs"></i><br>
+          <span class="hidden-xs">
+            <?php echo Yii::t("classified", "Publier"); ?>
+          </span>
+        </button>
+      </div>
+  </div>
+   <hr class="col-md-12 col-sm-12 col-xs-12 margin-top-10 margin-bottom-10 no-padding" id="before-section-result">
 </div>
 
-<div class="col-xs-12"><hr class="col-xs-12 col-sm-10 col-md-10"></div>
 
 <div class="col-lg-2 col-md-2 col-sm-3 col-xs-8 margin-top-15 text-right subsub classifiedFilters font-montserrat" id="sub-menu-left">
   <button class="open-type-filter tooltips" data-toggle="tooltip" data-placement="right" data-title="<?php echo Yii::t("common","Open filtering by type") ?>"><i class="fa fa-chevron-right"></i></button>
